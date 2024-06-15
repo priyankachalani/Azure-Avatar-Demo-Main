6e81c3052f135d6dc91fa0b8ecc4dac782ca1e75
chalani <chalanipriyanka.ch@gmail.com>
2024-06-15 14:33:23 +0530

    Initial commit for Azure Avatar Demo

---
+++ azure-avatar-demo-main/src/components/config.js
@@ -0,0 +1,12 @@
+
+export const avatarAppConfig = {
+    cogSvcRegion : "westus2",
+    cogSvcSubKey : "4a6a8fbbe8d54b019305fe9b01179f72",
+    voiceName : "en-US-JennyNeural",
+    avatarCharacter : "lisa",
+    avatarStyle : "casual-sitting",
+    avatarBackgroundColor : "#FFFFFFFF",
+    iceUrl : "stun:relay.communication.microsoft.com:3478",
+    iceUsername : "BQAANkrGOYAB2og1AimtI/H9ffNBUpjn7+pMJDbrUJQAAAAMARDaL+bjK3tPb7FB2K2TbIOJcoQnIm9JROiquFfg9hDD/lHNh4w=",
+    iceCredential : "gT8R7hwLRTJ3EPhhOHMufO7yLH0="
+  }
